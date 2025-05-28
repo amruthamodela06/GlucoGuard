f2103da6367281e4471a7e688b051d687f771e68
Sathvikaaaaaa-m <sathvika13406@gmail.com>
2025-05-28 19:24:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -243,5 +243,5 @@ with app.app_context():
 import os
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT not set
+    port = int(os.environ.get("PORT", 10000))  # Default to 5000 if PORT not set
     app.run(host='0.0.0.0', port=port)
